dea14de3a260bee010283e76d59b1d77092f04e6
ana-g0019 <ana.almeida.s0006@gmail.com>
2025-03-21 14:35:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function redirecionar() {
+    window.location.href = "yes.html";
+}
+
+document.getElementById("no-button").addEventListener("mouseover", function() {
+    this.style.position = "absolute";
+    this.style.top = Math.random() * window.innerHeight + "px";
+    this.style.left = Math.random() * window.innerWidth + "px";
+});
+
+document.getElementById("no-button").addEventListener("touchstart", function() {
+    this.style.position = "absolute";
+    this.style.top = Math.random() * window.innerHeight + "px";
+    this.style.left = Math.random() * window.innerWidth + "px";
+});
